31dad2f78712919fb5497483e8be625bee01e055
Eneko <eneko.codes@icloud.com>
2025-03-26 01:04:15 +0100

    Remove alert count display from alerts toggle component to streamline UI

--- resources/views/livewire/alerts/alerts-toggle.blade.php
+++ resources/views/livewire/alerts/alerts-toggle.blade.php
@@ -9,12 +9,6 @@
           <path d="M2 2a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1zm12-1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2z"/>
           <path d="M13 4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1z"/>
         </svg>
-        
-        @if ($this->alertCount > 0)
-          <span class="absolute -top-1 -right-1 flex items-center justify-center h-5 min-w-5 px-1 rounded-full bg-red-500 text-[11px] font-bold text-white">
-            {{ $this->alertCount > 99 ? '99+' : $this->alertCount }}
-          </span>
-        @endif
       </button>
     </x-tooltip>
 </div>
